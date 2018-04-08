aa0a45274ca943fcd05f77a2b1bda23b30b89e16
rsheth972 <rsheth972@gmail.com>
2018-04-08 12:52:24 +0530

    changed font

--- Userpage.php
+++ Userpage.php
@@ -224,4 +224,155 @@ function opensecurity(evt, securityName) {
 document.getElementById("defaultOpen").click();
             </script>
 </body>
+
+<?php
+	require_once("inc/connection.inc.php");
+
+	// Initialising variables to store extracted information
+	$name = [];
+	$symbol = [];
+	$exchange = [];
+	$open = [];
+	$close = [];
+	$high = [];
+	$low = [];
+	$lastprice = [];
+	$y = 0;
+	$z = '';
+	$key = "93dcc722279c3a7577f248b09ef6167f";
+
+
+	// Retreiving information from database
+	$sql = "SELECT * FROM portfolio";
+	$result = mysqli_query($conn, $sql);
+
+	// Check if databse is empty
+	if (mysqli_num_rows($result) > 0) 
+	{
+		while($row = mysqli_fetch_assoc($result)) 
+		{
+			$sym[$y] = $row["stocks_symbol"];
+			$pri[$y] = $row["price"];
+			$vol[$y] = $row["volume"];
+			$y += 1;
+		}
+	}
+	// If database empty
+	else 
+	{
+		?><h1><center><?php
+		echo "Portfolio Empty";
+		?></h1></center><?php
+		die();
+	}
+	mysqli_close($conn);
+
+	// Adding all stocks names in one variable to enable API call
+	for($a=0;$a<$y;$a++)
+	{
+		$z = $z.$sym[$a].',';
+	}
+
+	$z = rtrim($z,",");
+
+	// API call
+	$contents = file_get_contents("http://marketdata.websol.barchart.com/getQuote.json?key=$key&symbols=$z&mode=R");
+	$contents = json_decode($contents, true);
+
+	// Check successfull API call
+	if($contents["status"]["code"] == 200) 
+	{
+		foreach($contents['results'] as $result) 
+		{
+			array_push($name,$result['name']);
+			array_push($symbol,$result['symbol']);
+			array_push($exchange,$result['exchange']);
+			array_push($open,$result['open']);
+			array_push($close,$result['close']);
+			array_push($high,$result['high']);
+			array_push($low,$result['low']);
+			array_push($lastprice,$result['lastPrice']);
+		}
+	}
+	// If API call unsuccessful
+	else 
+	{
+		?>
+		<h1><center>"Error retreiving data. Please try again later."</center></h1>
+		<?php
+		die();
+	}
+?>
+
+<!-- Generating Output in tabular format -->
+<div id="modify_stock" class="tabcontent">
+<table class='table table-responsive'>
+	<tr class='head warning'>
+		<td>Name</td>
+		<td>Symbol</td>
+		<td>Exchange</td>
+		<td>Open</td>
+		<td>Close</td>
+		<td>High</td>
+		<td>Low</td>
+		<td>Last Price</td>
+		<td>Price Bought</td>
+		<td>Volume Bought</td>
+		<td>Change Per Stock</td>
+		<td>Profit/Loss</td>
+	</tr>
+	<?php
+		for($x=0;$x<$y;$x++) 
+		{?>
+			<tr>
+				<td><?php echo $name[$x]; ?></td>
+				<td><?php echo $symbol[$x]; ?></td>
+				<td><?php echo $exchange[$x]; ?></td>
+				<td><?php echo $open[$x]; ?></td>
+				<td><?php echo $close[$x]; ?></td>
+				<td><?php echo $high[$x]; ?></td>
+				<td><?php echo $low[$x]; ?></td>
+				<td><?php echo $lastprice[$x]; ?></td>
+				<td><?php echo $pri[$x]; ?></td>
+				<td><?php echo $vol[$x]; ?></td>
+
+				<td><?php 
+					if($pri[$x] > $lastprice[$x]) 
+					{?>
+						<i class="fa fa-arrow-down">
+						<?php echo $lastprice[$x]-$pri[$x];
+					}
+					else if($pri[$x] < $lastprice[$x]) 
+					{?>
+						</i>
+						<i class="fa fa-arrow-up">
+						<?php echo $lastprice[$x]-$pri[$x];
+						?></i><?php
+					}
+					else
+						echo '0';
+					?></td>
+				
+				<td><?php 
+					if($pri[$x] > $lastprice[$x])
+					{?>
+						<i class="fa fa-arrow-down">
+						<?php echo ($lastprice[$x]-$pri[$x]) * $vol[$x];
+					}
+					else if($pri[$x] < $lastprice[$x]) 
+					{?>
+						</i>
+						<i class="fa fa-arrow-up">
+						<?php echo ($lastprice[$x]-$pri[$x]) * $vol[$x];
+						?></i><?php
+					}
+					else
+						echo '0'; ?>
+				</td>
+			</tr>
+		<?php } ?>
+</table>
+                </div>
+<br>
+<br>
 </html>
